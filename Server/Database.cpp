a35ec5657173da7980a85e5fd553eeb283428ba1
SebiSomu <sebigsomu@gmail.com>
2025-11-12 19:44:17 +0200

    Created Database table

--- Server/Database.cpp
+++ Server/Database.cpp
@@ -1,1 +1,6 @@
 #include "Database.h"
+
+Database::Database(const std::string& path) : storage(initStorage(path)), dbPath(path)
+{
+	storage.sync_schema();
+}
--- Server/Database.h
+++ Server/Database.h
@@ -1,13 +1,28 @@
 #pragma once
 #include <sqlite_orm/sqlite_orm.h>
+#include "../THE-GAME/UserModel.h"
 
 using namespace sqlite_orm;
 
+inline auto initStorage(const std::string& path) {
+    return make_storage(path,
+        make_table("users",
+            make_column("id", &UserModel::id, primary_key().autoincrement()),
+            make_column("username", &UserModel::username, unique()),
+            make_column("password", &UserModel::password))
+    );
+}
+
+using Storage = decltype(initStorage(""));
+
 class Database
 {
-public:
-
 private:
+    Storage storage;
+    std::string dbPath;
+
+public:
+    Database(const std::string& path = "users.db");
 
 };
 
