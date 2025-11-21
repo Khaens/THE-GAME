4e839cc0bb280acc4ddbe9b626a80d0cccb5f01c
Mihnea Milescu <mihneamilescu@gmail.com>
2025-11-21 21:25:02 +0200

    Cleaned up app

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,16 +1,20 @@
 #include <crow.h>
 #include "GameServer.h"
 #include "Database.h"
+#include <memory>
 
 #include "Gambler.h"
 int main() {
-	//Game game(3);
-	//game.StartGame();
 	crow::SimpleApp app;
 
+	Database db("users.db");
+
+
 	CROW_ROUTE(app, "/")([]() {
-		return "Hello, World!";
+		return crow::response(200, "THE GAME Server is running!");
 		});
+
+
 
 	app.port(18080).multithreaded().run();
 
