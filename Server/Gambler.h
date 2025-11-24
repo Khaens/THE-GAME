71f43ba53610d0a578a1bfd7b0d8c98eac02a695
Mihnea Milescu <mihneamilescu@gmail.com>
2025-11-24 17:58:19 +0200

    Changed 1 variable

--- Server/Gambler.h
+++ Server/Gambler.h
@@ -12,6 +12,6 @@ public:
 	void UseAbility(TurnContext& ctx, size_t currentPIndex);
 	bool CanUseAbility(TurnContext& ctx) const;
 private:
-	size_t m_usesLeft = 2;
+	size_t m_usesLeft = 3;
 };
 
--- THE-GAME/x64/Debug/qt/moc/moc_SettingsDialog.cpp
+++ THE-GAME/x64/Debug/qt/moc/moc_SettingsDialog.cpp
@@ -7,7 +7,6 @@
 *****************************************************************************/
 
 #include "../../../../SettingsDialog.h"
-#include <QtGui/qtextcursor.h>
 #include <QtCore/qmetatype.h>
 
 #include <QtCore/qtmochelpers.h>
