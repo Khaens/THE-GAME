51341032d6aec083ff64916cc74802a07ac24b0f
Rolland Mămăligoi <rmamaligoi@gmail.com>
2025-11-29 17:41:43 +0200

    Add lobby status and start functionality to Lobby
    Introduced a LobbyStatus enum to track lobby state and added m_status and m_lobbyCreator members to the Lobby class. Implemented GetStatus() and Start() methods, updating the lobby status and starting the game when enough users have joined. Also updated JoinLobby to set the lobby creator when the first user joins.

--- Server/Lobby.h
+++ Server/Lobby.h
@@ -1,16 +1,25 @@
 #pragma once
 #include "GameServer.h"
 #include "Database.h"
+
+enum class LobbyStatus {
+	Waiting,
+	Started
+};
+
 class Lobby
 {
 private:
 	std::string m_id;
 	std::vector<UserModel> m_Users;
-	Game* m_game = nullptr;
+	UserModel m_lobbyCreator;
 	Database* m_db;
+	LobbyStatus m_status = LobbyStatus::Waiting;
 	static std::string GenerateRandomId(size_t length = 8);
 public:
 	Lobby(Database* db);
 	void JoinLobby(std::string playerName);
+	LobbyStatus GetStatus();
+	void Start();
 };
 
